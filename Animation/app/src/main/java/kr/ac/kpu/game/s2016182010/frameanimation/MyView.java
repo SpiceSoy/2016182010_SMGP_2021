246717e60797e5e6943d51a39b4f937e78c89e10
SpiceSoy <aksghkgh12@gmail.com>
2021-03-26 11:20:20 +0900

    add custom control (MyView)

--- Animation/app/src/main/java/kr/ac/kpu/game/s2016182010/frameanimation/MyView.java
+++ Animation/app/src/main/java/kr/ac/kpu/game/s2016182010/frameanimation/MyView.java
@@ -1,4 +1,27 @@
 package kr.ac.kpu.game.s2016182010.frameanimation;
 
-public class MyView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.View;
+
+public class MyView extends View {
+    private static final String TAG = MyView.class.getSimpleName();
+
+    // 일단은 activity = context
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas); 필요없음
+        Paint paint = new Paint();
+        paint.setColor(0xFF0044FF);
+        Rect rect = new Rect(10,20,300,400);
+        Log.d(TAG, "drawing " + rect);
+        canvas.drawRect(rect, paint);
+    }
 }
