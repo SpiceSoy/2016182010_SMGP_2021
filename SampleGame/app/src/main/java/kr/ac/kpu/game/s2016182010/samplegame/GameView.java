a4da2a7f121ed54aeef353cad032431a82591b22
SpiceSoy <aksghkgh12@gmail.com>
2021-03-29 11:56:22 +0900

    add First GameView

---
+++ SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182010/samplegame/GameView.java
@@ -0,0 +1,51 @@
+package kr.ac.kpu.game.s2016182010.samplegame;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    private static final String TAG = GameView.class.getSimpleName();
+    private Bitmap bitmap;
+    private float x;
+    private float y;
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initResources();
+        startUpdating();
+    }
+
+    private void initResources() {
+        Resources res = getResources();
+        bitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+        x = 100;
+        y = 100;
+    }
+
+    private void startUpdating() {
+        doGameFrameLoop();
+    }
+
+    private void doGameFrameLoop() {
+//        update();
+        x += 0.01f;
+        y += 0.02f;
+
+//        draw();
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Log.d(TAG, "Drawing at : " + x + ", " + y);
+        canvas.drawBitmap(bitmap, x, y, null);
+    }
+}
