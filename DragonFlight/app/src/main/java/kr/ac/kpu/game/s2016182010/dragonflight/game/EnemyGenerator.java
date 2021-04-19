e48d0c34e2953c1432c169d9badb3579db85ed5f
SpiceSoy <aksghkgh12@gmail.com>
2021-04-19 12:04:00 +0900

    add enemey generator generate 6 enemy

--- DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/EnemyGenerator.java
+++ DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/EnemyGenerator.java
@@ -4,6 +4,7 @@ import android.graphics.Canvas;
 import android.util.Log;
 
 import kr.ac.kpu.game.s2016182010.dragonflight.framework.GameObject;
+import kr.ac.kpu.game.s2016182010.dragonflight.ui.view.GameView;
 
 public class EnemyGenerator implements GameObject {
     public static final float INITIAL_SPAWN_INTERVAL = 5.0f;
@@ -15,6 +16,7 @@ public class EnemyGenerator implements GameObject {
         this.spawnInterval = INITIAL_SPAWN_INTERVAL;
         this.time = INITIAL_SPAWN_INTERVAL;
     }
+
     @Override
     public void update() {
         MainGame game = MainGame.get();
@@ -27,9 +29,15 @@ public class EnemyGenerator implements GameObject {
 
     private void generate() {
         Log.d(TAG, "Generate Now !!");
-        Enemy enemy = new Enemy(500, 0,700);
+
         MainGame game = MainGame.get();
-        game.add(enemy);
+        float sixth = GameView.instance.getWidth() / 6;
+        for (int i = 1; i <= 5; ++i) {
+            float x = sixth * i;
+            int y = 0;
+            Enemy enemy = new Enemy(x, y, 700);
+            game.add(enemy);
+        }
     }
 
     @Override
