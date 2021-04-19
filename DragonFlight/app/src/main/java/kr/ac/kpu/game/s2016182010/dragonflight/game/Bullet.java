e3714c1f6a000bdbc78b8365a1ad3a7d40200354
SpiceSoy <aksghkgh12@gmail.com>
2021-04-19 11:18:36 +0900

    fix using GameBitmap instance

--- DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/Bullet.java
+++ DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/Bullet.java
@@ -1,50 +1,37 @@
 package kr.ac.kpu.game.s2016182010.dragonflight.game;
 
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.RectF;
 
 import kr.ac.kpu.game.s2016182010.dragonflight.R;
 import kr.ac.kpu.game.s2016182010.dragonflight.framework.GameBitmap;
 import kr.ac.kpu.game.s2016182010.dragonflight.framework.GameObject;
-import kr.ac.kpu.game.s2016182010.dragonflight.ui.view.GameView;
 
 public class Bullet implements GameObject {
     private final float x;
-    private final Bitmap bitmap;
+    private final GameBitmap bitmap;
     private float y;
     private final int speed;
-    private final float hw;
-    private final float hh;
 
     public Bullet(float x, float y, int speed) {
         this.x = x;
         this.y = y;
         this.speed = speed;
 
-        bitmap = GameBitmap.load(R.mipmap.laser_1);
-
-        hw = bitmap.getWidth() / 2;
-        hh = bitmap.getHeight() / 2;
+        bitmap = new GameBitmap(R.mipmap.laser_1);
     }
 
     @Override
     public void update() {
         MainGame game = MainGame.get();
         y += -speed * game.frameTime;
+
+        if(y < 0 - this.bitmap.getHeight()) {
+            game.remove(this);
+        }
     }
 
     @Override
     public void draw(Canvas canvas) {
-//        float l = this.x - hw;
-//        float t = this.y - hh;
-//        canvas.drawBitmap(bitmap, l, t, null);
-
-        float left = this.x - hw * GameView.MULTIPLIER;
-        float top = this.y - hh * GameView.MULTIPLIER;
-        float right = this.x + hw * GameView.MULTIPLIER;
-        float bottom = this.y + hh * GameView.MULTIPLIER;
-        RectF dstRect = new RectF(left, top, right, bottom);
-        canvas.drawBitmap(bitmap, null, dstRect, null);
+        bitmap.draw(canvas, this.x, this.y);
     }
 }
--- DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/Player.java
+++ DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/Player.java
@@ -1,20 +1,14 @@
 package kr.ac.kpu.game.s2016182010.dragonflight.game;
 
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Rect;
-import android.graphics.RectF;
 
 import kr.ac.kpu.game.s2016182010.dragonflight.R;
 import kr.ac.kpu.game.s2016182010.dragonflight.framework.GameBitmap;
 import kr.ac.kpu.game.s2016182010.dragonflight.framework.GameObject;
-import kr.ac.kpu.game.s2016182010.dragonflight.ui.view.GameView;
 
 public class Player implements GameObject {
     private static final int BULLET_SPEED = 1500;
-    private Bitmap bitmap;
-    private int imageWidth;
-    private int imageHeight;
+    private GameBitmap bitmap;
     private float speed;
 
     private float x;
@@ -23,9 +17,7 @@ public class Player implements GameObject {
     private float tx;
 
     public Player(float x, float y) {
-        bitmap = GameBitmap.load(R.mipmap.fighter);
-        this.imageWidth = bitmap.getWidth();
-        this.imageHeight = bitmap.getHeight();
+        bitmap = new GameBitmap(R.mipmap.fighter);
         this.speed = 1000;
         this.x = x;
         this.y = y;
@@ -51,15 +43,6 @@ public class Player implements GameObject {
 
     @Override
     public void draw(Canvas canvas) {
-        float hw = imageWidth / 2;
-        float hh = imageHeight / 2;
-
-        float left = this.x - hw * GameView.MULTIPLIER;
-        float top = this.y - hh * GameView.MULTIPLIER;
-        float right = this.x + hw * GameView.MULTIPLIER;
-        float bottom = this.y + hh * GameView.MULTIPLIER;
-//        Rect srcRect = new Rect(0, 0, imageWidth, imageHeight);
-        RectF dstRect = new RectF(left, top, right, bottom);
-        canvas.drawBitmap(bitmap, null, dstRect, null);
+        bitmap.draw(canvas, this.x, this.y);
     }
 }
