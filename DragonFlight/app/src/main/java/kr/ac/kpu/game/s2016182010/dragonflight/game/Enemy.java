a22bd96c5d2594357a3aa1bd108057051e3c6e89
SpiceSoy <aksghkgh12@gmail.com>
2021-04-23 11:12:34 +0900

    add random level enemy

--- DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/Enemy.java
+++ DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/Enemy.java
@@ -14,14 +14,23 @@ public class Enemy implements GameObject, BoxCollidable {
     private final float x;
     private final AnimationGameBitmap bitmap;
     private float y;
+    private final int level;
     private final int speed;
 
-    public Enemy(float x, float y, int speed) {
+    private static final int RESOURCE_IDS[] = {
+            R.mipmap.enemy_01,R.mipmap.enemy_02,R.mipmap.enemy_03,R.mipmap.enemy_04,R.mipmap.enemy_05,
+            R.mipmap.enemy_06,R.mipmap.enemy_07,R.mipmap.enemy_08,R.mipmap.enemy_09,R.mipmap.enemy_10,
+            R.mipmap.enemy_11,R.mipmap.enemy_12,R.mipmap.enemy_13,R.mipmap.enemy_14,R.mipmap.enemy_15,
+            R.mipmap.enemy_16,R.mipmap.enemy_17,R.mipmap.enemy_18,R.mipmap.enemy_19,R.mipmap.enemy_20
+    };
+
+    public Enemy(int level, float x, float y, int speed) {
+        this.level = level;
         this.x = x;
         this.y = y;
         this.speed = speed;
-
-        bitmap = new AnimationGameBitmap(R.mipmap.enemy_01, FRAMES_PER_SECOND, 0);
+        int resId = RESOURCE_IDS[level - 1];
+        bitmap = new AnimationGameBitmap(resId, FRAMES_PER_SECOND, 0);
     }
 
     @Override
--- DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/EnemyGenerator.java
+++ DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/game/EnemyGenerator.java
@@ -3,6 +3,8 @@ package kr.ac.kpu.game.s2016182010.dragonflight.game;
 import android.graphics.Canvas;
 import android.util.Log;
 
+import java.util.Random;
+
 import kr.ac.kpu.game.s2016182010.dragonflight.framework.GameObject;
 import kr.ac.kpu.game.s2016182010.dragonflight.ui.view.GameView;
 
@@ -35,7 +37,9 @@ public class EnemyGenerator implements GameObject {
         for (int i = 1; i <= 9; i+=2) {
             float x = tenth * i;
             int y = 0;
-            Enemy enemy = new Enemy(x, y, 700);
+            Random r = new Random();
+            int level = r.nextInt(20) + 1;
+            Enemy enemy = new Enemy(level, x, y, 700);
             game.add(enemy);
         }
     }
