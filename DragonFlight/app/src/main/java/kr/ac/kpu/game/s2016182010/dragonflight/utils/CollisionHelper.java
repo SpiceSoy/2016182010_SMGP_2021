43a8e0f23303475954d0cbe9a720787ce080b591
SpiceSoy <aksghkgh12@gmail.com>
2021-04-23 11:04:22 +0900

    fix collision error

--- DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/utils/CollisionHelper.java
+++ DragonFlight/app/src/main/java/kr/ac/kpu/game/s2016182010/dragonflight/utils/CollisionHelper.java
@@ -13,9 +13,9 @@ public class CollisionHelper {
         a.getBoundingRect(rect1);
         b.getBoundingRect(rect2);
         if (rect1.left > rect2.right) return false;
-        if (rect1.left > rect2.right) return false;
-        if (rect1.left > rect2.right) return false;
-        if (rect1.left > rect2.right) return false;
+        if (rect1.top > rect2.bottom) return false;
+        if (rect1.right < rect2.left) return false;
+        if (rect1.bottom < rect2.top) return false;
         return true;
     }
 }
